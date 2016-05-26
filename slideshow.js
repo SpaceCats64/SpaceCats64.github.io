381bfe0fd7e9f5df6a83052103d9cb6295c18e01
SpaceCats64 <spacecats64@gmail.com>
2016-05-25 23:31:52 -0400

    Use jQeury to make some things look nicer
    Decided to make the slide show images look a little nicer and (if
    jquery's get works with github pages) get the latest video from my
    youtube channel.

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,46 @@
 var index = 0;
 var imgList = ["http://puu.sh/llWhI/bb34af74fc.png", "http://puu.sh/llWdT/8fe6eeefa9.png", "http://puu.sh/l1cnC/9570ede389.png", 
  "http://puu.sh/kIeP1/8c6af8c7f8.jpg"];
-var srcSet = function(){
-	if(index <= imgList.length - 1){
-		document.getElementById('slideshow').src = imgList[index+1];
-		index += 1;
-	}
-	if(index == imgList.length){
-		document.getElementById('slideshow').src = imgList[0];
-		index = 0;
-	}
-}
-//document.getElementById("Slsleft").addEventListener("click", srcSet());
-//document.getElementById("Slsright").addEventListener("click", srcSet());
+ $(document).ready(() => {
+	$('#slideshow').click(()=>{
+		$('#slideshow').fadeOut(600, () => {
+			index += 1;
+			if (index == imgList.length) {
+				index = 0;
+			}
+			$('#slideshow').attr('src', imgList[index]).fadeIn(600);
+		});
+	});
+
+	getLatestVideo();
+
+	function getLatestVideo(){
+		$.get(
+			"https://www.googleapis.com/youtube/v3/channels", {
+				part: 'contentDetails',
+				id: 'UCzaQEP2Se2L0Tx5O0UyZdUg',
+				key: 'AIzaSyDHhN8ZabTXdziXf_NjZQD00N8bDDFgSEY'},
+				(data) =>{
+					$.each(data.items, (i, item) => {
+						p = item.contentDetails.relatedPlaylists.uploads;
+						return getVid(p);
+					});
+				}
+		);
+	};
+
+	function getVid (pl) {
+		$.get(
+			"https://www.googleapis.com/youtube/v3/playlistItems", {
+			part: 'snippet',
+			maxResults: 1,
+			playlistId: pl,
+			key: 'AIzaSyDHhN8ZabTXdziXf_NjZQD00N8bDDFgSEY'},
+			(data) =>{
+				$.each(data.items, (i, item) => {
+					$('#lVideo').attr('src', "https://www.youtube.com/embed/" + item.snippet.resourceId.videoId);
+				});
+			}
+		);
+	};
+});
