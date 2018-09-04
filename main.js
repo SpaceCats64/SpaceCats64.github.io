9eece7dd3bc27aff0bd45c7d821df2e0ad5cc9e0
SpaceCats64 <spacecats64@gmail.com>
2018-09-03 23:05:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$.ajax({url:"https://github.com/SpaceCats64", crossDomain: true, type:"GET"}).done(function(res){
+$.ajax({url:"https://github.com/SpaceCats64", crossDomain: true, type:"GET", xhrFields: {withCredentials: true}}).done(function(res){
     $(res.responseText).find('.pinned-repo-item-content').each((index, repo)=>{
         repoName = $(repo).find(".repo").first().text();
         repoDesc = $(repo).find(".pinned-repo-desc").first().text();
@@ -7,5 +7,5 @@ $.ajax({url:"https://github.com/SpaceCats64", crossDomain: true, type:"GET"}).do
         $("#pinnedRepos").append('<div class="mdl-grid-cell mdl-cell--3-col"><div class="mdl-card mdl-shadow--3dp projectCard"><h3><a style="color: rgba(0,0,0,.87);" href="https://github.com/SpaceCats64/' + repoName + '">'+repoName+'</a></h3><hr><p>'+repoDesc+'<br>Language: '+repoData[0]+'<span style="margin-left: 5%">Stars: '+(repoData[1] == undefined ? 0 : repoData[1])+'</span></p></div></div>');
     });
 }).fail(function(){
-    $("#pinnedRepos").append('<div class="mdl-grid-cell mdl-cell--3-col"><div class="mdl-card mdl-shadow--3dp projectCard"><h3>Error Loading Pinned Repositories, try reloading...</h3></div>');
+    $("#pinnedRepos").append('<div class="mdl-grid-cell mdl-cell--5-col"><div class="mdl-card mdl-shadow--3dp projectCard"><h3>Error Loading Pinned Repositories, try reloading...</h3></div>');
 });
