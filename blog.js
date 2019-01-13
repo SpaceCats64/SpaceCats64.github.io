e4883b8674828706962f98bcd90254d054cc7a4f
SpaceCats64 <spacecats64@gmail.com>
2019-01-13 00:21:26 -0500

    switch to using gatsby.js for static site generation and pray it works

--- blog.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(()=>{
-	$(".postLink").click((e)=>{
-		$("#content").load($(e.target).attr("post"));
-	});
-	$("#back").click(()=>{
-		$("#content").load("blog.html #content");
-	});
-});
